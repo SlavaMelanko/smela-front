3c7ac718adda440be31c6e5826a4dafa19d25cc1
Slava Melanko <slava.melanko@gmail.com>
2025-04-10 09:41:21 +0300

    Fixed Auth layout

--- src/layouts/Auth/index.jsx
+++ src/layouts/Auth/index.jsx
@@ -2,6 +2,7 @@ import './styles.scss'
 
 import { Outlet } from 'react-router-dom'
 
+import { MassiveLogo } from '@/components/icons'
 import ThemeToggle from '@/components/ThemeToggle'
 
 const AuthLayout = () => {
@@ -11,8 +12,13 @@ const AuthLayout = () => {
         <ThemeToggle />
       </div>
 
-      <div className='auth-layout__content'>
-        <Outlet />
+      <div className='auth-layout__container'>
+        <div className='auth-layout__content'>
+          <div className='auth-layout__logo'>
+            <MassiveLogo width={240} height={45} />
+          </div>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
