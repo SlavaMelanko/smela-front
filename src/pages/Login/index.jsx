b10992471d6c78e6e050649bafa56ce23fb17a99
WilfredoN <thekillermaps@gmail.com>
2025-04-10 22:45:04 +0300

    refactor: apply AuthLayout changes to login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,8 +3,8 @@ import './styles.scss'
 import { useForm } from 'react-hook-form'
 
 import { GoogleLoginButton, PrimaryButton } from '@/components/Button'
+import FormField from '@/components/form/Field'
 import { PasswordInput, TextInput } from '@/components/inputs'
-import FormField from '@/components/inputs/Form'
 import Separator from '@/components/Separator'
 import {
   loginSchema,
@@ -35,45 +35,38 @@ const Login = () => {
 
   return (
     <div className='login-page'>
-      <div className='login-page__card'>
-        <div className='login-page__logo'>
-          <MassiveLogo width={240} height={45} />
-        </div>
-        <form className='login-page__form' onSubmit={handleSubmit(onSubmit)}>
-          <div className='login-page__fields'>
-            <FormField label='Email' name='email' error={errors.email?.message}>
-              <TextInput
-                name='email'
-                placeholder='example@email.com'
-                required={true}
-                error={errors.email?.message}
-                {...register('email')}
-              />
-            </FormField>
+      <form className='login-page__form' onSubmit={handleSubmit(onSubmit)}>
+        <div className='login-page__fields'>
+          <FormField label='Email' name='email' error={errors.email?.message}>
+            <TextInput
+              name='email'
+              placeholder='example@email.com'
+              error={errors.email?.message}
+              {...register('email')}
+            />
+          </FormField>
 
-            <FormField
-              label='Password'
+          <FormField
+            label='Password'
+            name='password'
+            error={errors.password?.message}
+          >
+            <PasswordInput
               name='password'
               error={errors.password?.message}
-            >
-              <PasswordInput
-                name='password'
-                required={true}
-                error={errors.password?.message}
-                {...register('password')}
-              />
-            </FormField>
-          </div>
-          <PrimaryButton type='submit'>Login</PrimaryButton>
-        </form>
-        <div className='login-page__separator'>
-          <Separator />
+              {...register('password')}
+            />
+          </FormField>
         </div>
-        <GoogleLoginButton />
-        <div className='login-page__prompts'>
-          <SignupPrompt />
-          <ForgotYourPasswordPrompt />
-        </div>
+        <PrimaryButton type='submit'>Login</PrimaryButton>
+      </form>
+      <div className='login-page__separator'>
+        <Separator />
+      </div>
+      <GoogleLoginButton />
+      <div className='login-page__prompts'>
+        <SignupPrompt />
+        <ForgotYourPasswordPrompt />
       </div>
     </div>
   )
