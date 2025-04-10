065c63e6f057737235856362a16590727bab7f3f
Slava Melanko <slava.melanko@gmail.com>
2025-04-10 21:33:54 +0300

    Updated styles a bit

--- src/components/inputs/Input/index.jsx
+++ src/components/inputs/Input/index.jsx
@@ -3,32 +3,18 @@ import './styles.scss'
 import clsx from 'clsx'
 
 const Input = ({
-  id,
   type = 'text',
-  placeholder = '',
-  required = false,
   autoComplete = 'off',
-  className = '',
-  rightElement = null,
-  onChange,
-  onBlur,
-  value,
-  name,
+  className,
+  rightElement,
   error,
   ...rest
 }) => {
   return (
     <div className='input'>
       <input
-        id={id}
         type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
         autoComplete={autoComplete}
-        required={required}
-        placeholder={placeholder}
         className={clsx(
           'input__field',
           { 'input__field--with-icon': !!rightElement },
