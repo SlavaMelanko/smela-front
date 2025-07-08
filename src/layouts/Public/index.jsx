7a497a9fa945749fe1d9da978ffd91d22ff3a006
Slava Melanko <slava.melanko@gmail.com>
2025-07-08 10:45:08 +0300

    Remove extra blank line in PublicLayout
    Deleted an unnecessary blank line after imports in src/layouts/Public/index.jsx to improve code cleanliness.

--- src/layouts/Public/index.jsx
+++ src/layouts/Public/index.jsx
@@ -7,6 +7,7 @@ import Copyright from '@/components/Copyright'
 import { LanguageSelector } from '@/components/dropdowns'
 import { Spinner } from '@/components/Spinner'
 import ThemeToggle from '@/components/ThemeToggle'
+
 const PublicLayout = () => {
   return (
     <div className='public-layout'>
