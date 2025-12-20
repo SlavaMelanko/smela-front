2db982f251f6588992850cee9df16f33da1c5cfe
Slava Melanko <slava.melanko@gmail.com>
2025-12-20 20:37:53 +0200

    Refactor Legal layout from SCSS to Tailwind

--- src/layouts/Legal/index.jsx
+++ src/layouts/Legal/index.jsx
@@ -1,5 +1,3 @@
-import './styles.scss'
-
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 
@@ -10,24 +8,24 @@ import Spinner from '@/components/Spinner'
 import ThemeToggle from '@/components/ThemeToggle'
 
 const LegalLayout = () => (
-  <div className='legal-layout'>
-    <div className='legal-layout__user-preferences'>
+  <div className='relative flex min-h-screen flex-col items-center justify-center bg-background px-4 py-16'>
+    <div className='absolute right-4 top-4 z-[4] flex gap-4 md:right-8 md:top-8'>
       <ThemeToggle />
       <LanguageDropdown />
     </div>
 
-    <div className='legal-layout__container'>
-      <header className='legal-layout__header'>
+    <div className='mx-auto flex w-full max-w-screen-lg grow flex-col items-center gap-8'>
+      <header className='flex items-center justify-center text-foreground'>
         <Logo width={280} />
       </header>
 
-      <main className='legal-layout__content'>
+      <main className='w-full grow p-4'>
         <Suspense fallback={<Spinner />}>
           <Outlet />
         </Suspense>
       </main>
 
-      <footer className='legal-layout__footer'>
+      <footer>
         <Copyright />
       </footer>
     </div>
