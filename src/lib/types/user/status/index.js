f04538f559eabfcf58a6120530fa79e4b708471e
Slava Melanko <slava.melanko@gmail.com>
2025-12-26 14:58:40 +0200

    Update index.js

--- src/lib/types/user/status/index.js
+++ src/lib/types/user/status/index.js
@@ -19,7 +19,7 @@ export const adminActiveStatuses = [UserStatus.ACTIVE]
 
 const statusToBgColor = {
   [UserStatus.NEW]: 'bg-sky-500',
-  [UserStatus.VERIFIED]: 'bg-emerald-500',
+  [UserStatus.VERIFIED]: 'bg-lime-500',
   [UserStatus.TRIAL]: 'bg-violet-500',
   [UserStatus.ACTIVE]: 'bg-green-500',
   [UserStatus.SUSPENDED]: 'bg-red-500',
@@ -32,7 +32,7 @@ export const getStatusBgColor = status =>
 
 const statusToTextColor = {
   [UserStatus.NEW]: 'text-sky-500',
-  [UserStatus.VERIFIED]: 'text-emerald-500',
+  [UserStatus.VERIFIED]: 'text-lime-500',
   [UserStatus.TRIAL]: 'text-violet-500',
   [UserStatus.ACTIVE]: 'text-green-500',
   [UserStatus.SUSPENDED]: 'text-red-500',
