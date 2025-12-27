32c41a17bb35a6f87982f902e1957722130c65b3
Slava Melanko <slava.melanko@gmail.com>
2025-12-27 12:07:59 +0200

    Migrate NotificationToggle from SCSS to Tailwind

--- src/components/notifications/Toggle/index.jsx
+++ src/components/notifications/Toggle/index.jsx
@@ -1,17 +1,22 @@
-import './styles.scss'
-
 import { BellIcon } from '@/components/icons'
 import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
 
 const NotificationToggle = ({ unreadCount = 0, onClick, ref }) => (
-  <button className='notification-toggle' onClick={onClick} ref={ref}>
+  <Button
+    variant='ghost'
+    size='icon'
+    className='relative rounded-full'
+    onClick={onClick}
+    ref={ref}
+  >
     <BellIcon size='md' />
     {unreadCount > 0 && (
-      <Badge variant='notification' className='notification-toggle__badge'>
+      <Badge variant='notification' className='absolute top-0.5 right-0'>
         {unreadCount > 9 ? '9+' : unreadCount}
       </Badge>
     )}
-  </button>
+  </Button>
 )
 
 export default NotificationToggle
