0f56ef349469ccc5a7cb1b08b1ed87592ae60077
Slava Melanko <slava.melanko@gmail.com>
2026-02-18 08:51:01 +0200

    Redirect to 404 when user has no team

--- src/pages/user/Team/TeamPage.jsx
+++ src/pages/user/Team/TeamPage.jsx
@@ -1,8 +1,14 @@
+import { Navigate } from 'react-router-dom'
+
 import { useCurrentUser } from '@/hooks/useAuth'
 import { TeamPage as SharedTeamPage } from '@/pages/shared/Team'
 
 export const TeamPage = () => {
   const { team } = useCurrentUser()
 
-  return <SharedTeamPage teamId={team?.id} />
+  if (!team) {
+    return <Navigate to='/not-found' replace />
+  }
+
+  return <SharedTeamPage teamId={team.id} />
 }
