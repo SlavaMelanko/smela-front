dd726b122f3043d0a599ce2c1032aa73923a4060
Slava Melanko <slava.melanko@gmail.com>
2026-03-01 17:58:15 +0200

    Add StatusDropdown component with Storybook stories

---
+++ src/components/StatusDropdown/StatusDropdown.jsx
@@ -0,0 +1,36 @@
+import { StatusBadge } from '@/components/badges'
+import { ChevronIcon } from '@/components/icons'
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger
+} from '@/components/ui'
+import { UserStatus } from '@/lib/types'
+import { cn } from '@/lib/utils'
+
+const statuses = Object.values(UserStatus)
+
+export const StatusDropdown = ({ className, value, onChange }) => (
+  <DropdownMenu>
+    <DropdownMenuTrigger
+      className={cn(className)}
+      render={<Button variant='outline' className='min-w-36 justify-between' />}
+    >
+      <StatusBadge status={value} />
+      <ChevronIcon className='hidden group-aria-expanded/button:rotate-180 sm:block' />
+    </DropdownMenuTrigger>
+
+    <DropdownMenuContent align='end' className='min-w-(--anchor-width)'>
+      <DropdownMenuRadioGroup value={value} onValueChange={onChange}>
+        {statuses.map(status => (
+          <DropdownMenuRadioItem key={status} value={status}>
+            <StatusBadge status={status} />
+          </DropdownMenuRadioItem>
+        ))}
+      </DropdownMenuRadioGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>
+)
---
+++ src/components/StatusDropdown/StatusDropdown.stories.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+import { UserStatus } from '@/lib/types'
+
+import { StatusDropdown } from '.'
+
+export default {
+  title: 'Components/StatusDropdown',
+  component: StatusDropdown,
+  parameters: { layout: 'fullscreen' },
+  decorators: [
+    Story => (
+      <div className='flex items-center justify-center w-full min-h-screen'>
+        <Story />
+      </div>
+    )
+  ]
+}
+
+const InteractiveStatusDropdown = ({ initialStatus }) => {
+  const [status, setStatus] = useState(initialStatus)
+
+  return <StatusDropdown value={status} onChange={setStatus} />
+}
+
+export const Default = {
+  render: () => <InteractiveStatusDropdown initialStatus={UserStatus.ACTIVE} />
+}
+
+export const New = {
+  render: () => <InteractiveStatusDropdown initialStatus={UserStatus.NEW} />
+}
+
+export const Suspended = {
+  render: () => (
+    <InteractiveStatusDropdown initialStatus={UserStatus.SUSPENDED} />
+  )
+}
