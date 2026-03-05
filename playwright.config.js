1bde682c9d767365a8c57faf7b83d18529d6e6f7
Slava Melanko <slava.melanko@gmail.com>
2026-03-05 10:32:23 +0200

    Fix Playwright HTML report path conflict

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   testDir: './e2e',
   globalSetup: './e2e/config/globalSetup.js',
   forbidOnly: !!process.env.CI,
-  reporter: [['html', { outputFolder: './e2e/artifacts/html-report' }]],
+  reporter: [['html', { outputFolder: './e2e/html-report' }]],
   outputDir: './e2e/artifacts',
   use: {
     baseURL: process.env.VITE_FE_BASE_URL || 'http://localhost:5173',
