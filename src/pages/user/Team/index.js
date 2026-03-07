97a471dea06aaa89cb64c302a47a0fde8336cb27
Slava Melanko <slava.melanko@gmail.com>
2026-03-07 23:15:07 +0200

    Consolidate team components into components/team

--- src/pages/user/Team/index.js
+++ src/pages/user/Team/index.js
@@ -1,3 +1,2 @@
 export { TeamInfoPage } from './TeamInfoPage'
 export { TeamMembersPage } from './TeamMembersPage'
-export { TeamPage } from './TeamPage'
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,3 +1,3 @@
 export { HomePage } from './Home'
 export { SettingsPage } from './Settings'
-export { TeamInfoPage, TeamMembersPage, TeamPage } from './Team'
+export { TeamInfoPage, TeamMembersPage } from './Team'
