880bcb05fc1142dede73e3b33187a7acef928fd0
Slava Melanko <slava.melanko@gmail.com>
2026-03-11 18:17:00 +0200

    Implement profile page with profile and membership tabs

---
+++ src/pages/user/Profile/ProfileTab.jsx
@@ -0,0 +1,8 @@
+import { ProfileSection } from '@/components/profile'
+import { useUpdateCurrentUser } from '@/hooks/useAuth'
+
+export const ProfileTab = ({ user }) => {
+  const { mutate, isPending } = useUpdateCurrentUser()
+
+  return <ProfileSection user={user} update={mutate} isUpdating={isPending} />
+}
