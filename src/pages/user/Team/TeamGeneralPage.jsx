e91e137b39c44d1a373faafcf8e476e2fa98838b
Slava Melanko <slava.melanko@gmail.com>
2026-03-12 09:38:57 +0200

    Rename TeamInfoPage to TeamGeneralPage and update route

--- src/pages/user/Team/TeamGeneralPage.jsx
+++ src/pages/user/Team/TeamGeneralPage.jsx
@@ -2,7 +2,7 @@ import { useOutletContext } from 'react-router-dom'
 
 import { TeamInfoSection } from '@/components/team'
 
-export const TeamInfoPage = () => {
+export const TeamGeneralPage = () => {
   const { team } = useOutletContext()
 
   return <TeamInfoSection team={team} />
